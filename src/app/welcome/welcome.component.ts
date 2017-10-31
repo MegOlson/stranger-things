57d715e3334c036691573fb9ebcddde18c459dbb
Kristen Kulha and Megan Olson <megan@>
2017-10-31 09:25:31 -0700

    Add styling to welcome component.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  fullImagePath: string;
+  constructor() {
+    this.fullImagePath = 'assets/images/welcome-stranger.png';
+  }
 
+  ngOnInit() {}
 }
